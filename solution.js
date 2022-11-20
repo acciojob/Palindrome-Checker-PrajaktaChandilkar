68431e8c9c77e3c302e75be63a8357991d7c5b2a
PrajaktaChandilkar <95705302+PrajaktaChandilkar@users.noreply.github.com>
2022-11-20 22:03:07 +0530

    done

--- solution.js
+++ solution.js
@@ -1,6 +1,22 @@
-// complete the given function
+function palindrome(str) {
+    // your code here!
+    let len = str.length;
+    str = str.toLowerCase();
+ 
+    for(let index=0;index<len/2;index++){
+        if(str[index]!==str[len-1-index]){
+            return false;
+        }
+    }
+    return true;
+    //code end. don't edit anything below
+  }
 
-function palindrome(str){
 
-}
 module.exports = palindrome
+// const str = prompt('Enter a string: ');
+
+// call the function
+const value = palindrome('racecar');
+
+console.log(value);
